459c0b031e29f0298a216ea93629a19a868eb7df
Noah Stasyszen <stasyszen@gmail.com>
2019-11-22 11:46:56 -0600

    Made this fit the project better

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,12 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeSet;
 //problem in LetterAvgtoString
 public class MesoInherit extends MesoStation
 {
-	protected static String[] stations = {"BYOB", "BOIS", "ITSA", "MEEE"};
-	protected static int size = 0;
+	protected static ArrayList<String> stations;
+	protected static TreeSet<String> sortedStations;
 	
 	public MesoInherit(String stId) 
 	{
@@ -15,7 +17,7 @@ public class MesoInherit extends MesoStation
 		
 		//try to read in so we have that data, there will be an array for every MesoInherit object
 		try {
-			MesoInherit.readIn();
+			readIn();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -36,66 +38,25 @@ public class MesoInherit extends MesoStation
 		//try to read in the file with a scanner
 		
 		try {
-			//reset the array so there are no duplicates
-			String[] clearArray = {"BYOB", "BOIS", "ITSA", "MEEE"};
-			stations = clearArray;
-			size = 0;
-			
 			String str = "Mesonet.txt";
 			File file = new File(str);
 			Scanner in = new Scanner(file);
 
-			//read in the first 2 unusable lines
-			in.nextLine();
-			in.nextLine();
-			
-
-			//set up some variables 
-			boolean hasInt = false;
-			String string1;
-			String string2;
+			String station;
 			
 			//while there is a next line...
 			while (in.hasNext()) {
-				hasInt = false;
-				string1 = in.next();//.next so that only one grouping is read in at a time
-				string2 = in.next();
+				//read in the string
+				station = in.next();
 				
-				//if there is not room for the next two items read in
-				if(size + 2 >= stations.length)
-				{
-					expand();
-				}
-
-				for (Integer i = 0; i < 10; ++i) {
-					// find out if the string has a number in it
-					if (string1.contains(i.toString())) {//checks to see if the string has any number from 0 to 9 in it
-						hasInt = true;
-					}
-				}
-
-				// if there was no int in the first string add it and try the second
-				if (hasInt == false) {
-					stations[size] = string1;
-					++size;
-					
-					
-					for (Integer i = 0; i < 10; ++i) {
-						// find out if the string has a number in it
-						if (string2.contains(i.toString())) {
-							hasInt = true;
-						}
-					}
-					//if second has an int, add it
-					if (hasInt == false) {
-						stations[size] = string2;
-						++size;
-					}
-
-				}
+				//add the station in
+				stations.add(station);
 			}
-
 			in.close();
+			
+			//now to make the sorted version of the stations
+			sortedStations = new TreeSet<String>(stations);
+			
 		}//close the scanner at the end of the try block for good luck
 
 		// catch the exception
@@ -109,30 +70,9 @@ public class MesoInherit extends MesoStation
 //		}
 	}
 	
-	private static void expand()
-	{
-		int newSize = size + 20;
-		String[] replacement = new String[newSize];
-		
-		//for the length of the stations, all extra spots will be null
-		for(int i = 0; i < stations.length; ++i)
-		{
-			replacement[i] = stations[i];
-		}
-		
-		stations = replacement;
-	}
-	
-	public int[] calAverage()
-	{
-		LetterAvg j = new LetterAvg(super.getStID());
-		return j.calAverage();
-	}
-	
-	public char letterAverage()
-	{
-		LetterAvg j = new LetterAvg(super.getStID());
-		return j.letterAverage();
+	//make sure we can add a station to the sorted list
+	public void addStation(String station) {
+		sortedStations.add(station);
 	}
 	
 }
